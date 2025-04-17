6feb23623fdef6b7bb4ca923238539f3b449c8ae
MT7784 <mezo.nassef@gmail.com>
2025-04-17 13:37:27 +0200

    implemented health header file

--- health.h
+++ health.h
@@ -1,10 +1,26 @@
 #ifndef HEALTH_H
 #define HEALTH_H
 
-class Health
-{
+class Health {
 public:
-    Health();
+    Health(int max = 3) : maxHealth(max), currentHealth(max) {}
+
+    void takeDamage(int amount = 1) {
+        currentHealth -= amount;
+        if (currentHealth < 0) currentHealth = 0;
+    }
+
+    void reset() {
+        currentHealth = maxHealth;
+    }
+
+    int get() const {
+        return currentHealth;
+    }
+
+private:
+    int maxHealth;
+    int currentHealth;
 };
 
 #endif // HEALTH_H
