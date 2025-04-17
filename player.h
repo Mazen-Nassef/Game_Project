908d9e605acf71cc9a0ccc57d284dd96a0d58263
MT7784 <mezo.nassef@gmail.com>
2025-04-17 13:36:26 +0200

    finished header files for player and health

--- player.h
+++ player.h
@@ -1,10 +1,40 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-class Player
-{
+#include <QObject>
+#include <QGraphicsRectItem>
+#include <QKeyEvent>
+#include <QTimer>
+#include "health.h"
+
+class Player : public QObject, public QGraphicsRectItem {
+    Q_OBJECT
 public:
     Player();
+
+    void keyPressEvent(QKeyEvent *event) override;
+
+    // Movement
+    void moveForward();
+    void moveBackward();
+    void jump();
+
+    // Game Mechanics
+    void takeDamage();
+    void reset();
+
+private slots:
+    void applyGravity();
+
+private:
+    Health health;
+    float speed;
+    int length;
+    int width;
+    int jumpHeight;
+    int yVelocity;
+
+    QTimer *gravityTimer;
 };
 
 #endif // PLAYER_H
