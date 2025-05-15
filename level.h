a74a9a07d6f2b0387fb56a26e8818f2683a6ea4f
Michael10504 <michaelredaa.2005@gmail.com>
2025-05-15 21:13:47 +0300

    handled collisions with enemy

--- level.h
+++ level.h
@@ -38,13 +38,13 @@ private:
     QGraphicsScene* scene;
     QList<Platform*> platforms;
     QList<Obstacle*> obstacles;
-     QList<Enemy*> enemies;
+    QList<Enemy*> enemies;
     QGraphicsRectItem* goal;
     Player* player;
-    
+
     // Clear level elements
     void clearLevel();
-    
+
     // Level setup methods
     void setupLevel1();
     void setupLevel2();
@@ -55,16 +55,16 @@ private:
 public:
     Level(QGraphicsScene* scene = nullptr);
     ~Level();
-    
+
     // Configure the level
     void setLevelType(LevelType type);
     void setScene(QGraphicsScene* scene);
 
     void removeEnemy(Enemy* enemy);
-    
+
     // Create the level with the current configuration
     void createLevel();
-    
+
     // Level properties getters
     int getLevelNumber() const;
     double getFrequencyM() const;
@@ -72,13 +72,13 @@ public:
     double getDamageM() const;
     int getWidth() const;
     int getHeight() const;
-    
+
     // Update to next level
     void update_level();
 
     // Camera control
     void followPlayer(QGraphicsView* view);
-     void checkFlagCollision();
+    void checkFlagCollision();
     // Get the player pointer
     Player* getPlayer() const { return player; }
 };
